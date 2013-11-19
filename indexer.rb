33242132b24b4bb8eaa56a5f29054bc5b204a803
Radulfr <raul.elfriky@gmail.com>
2013-11-19 20:30:55 +0100

    Adding terms to the terms table

--- indexer.rb
+++ indexer.rb
@@ -9,16 +9,30 @@ class Indexer
     @db         = @connection.db("ARI")
     @stopwords  = @db.collection("stopWords")
     @docs       = @db.collection("documents")
-    @terms      = @db.collection("terms")
+    @termscoll  = @db.collection("terms")
     @sw         = getSW
+    @terms      = getIndexedTerms
   end
 
-#Recover the StopWords list
+  def getIndexedTerms
+    @terms = Array.new
+    indexedTerms = @termscoll.find.to_a
+    if indexedTerms.size == 0
+      return 0
+    else
+      indexedTerms.size.times { |i| @terms[i] = indexedTerms[i].to_s.scan(/"[a-z]+"=>/)}
+      @terms.size.times { |i| @terms[i].to_s.scan(/\w+/){ |w| @terms[i] = w} }
+      return @terms
+    end
+  end
+
+  #Recover the StopWords list
   def getSW
     @sw = Array.new
     stopWords = @stopwords.find.to_a
     stopWords.size.times { |i| @sw[i] = stopWords[i].to_s.scan(/=>"[a-z]+"/)}
-    @sw.size.times { |i| @sw[i] = @sw[i].to_s.scan(/\w+/)}
+#    @sw.size.times { |i| @sw[i] = @sw[i].to_s.scan(/\w+/)}
+    @sw.size.times { |i| @sw[i].to_s.scan(/\w+/){ |w| @sw[i] = w} }
     return @sw
   end
 
@@ -31,12 +45,21 @@ class Indexer
     end
     content.encode!('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
      content = content.scan(/\S\w+\D/)
-    content.size.times { |i| content[i] = [content[i].gsub(/\p{^Alnum}/, '').to_s]}
+    content.size.times { |i| content[i] = content[i].gsub(/\p{^Alnum}/, '').to_s}
 #    @sw.size.times { |i| puts @sw[i].to_s + " - " + content[i].to_s}
-    content = content - @sw
-#    puts content.size.to_s
 
-    content.each { |doc| puts doc.to_s}
+    #Deleting StopWords
+    content = content - @sw
+    self.getIndexedTerms
+
+    for i in 0..content.size-1
+      if not @terms.include?(content[i])
+        @termscoll.insert(content[i] => 0)
+      else
+      end
+    end
+#    content.each { |doc| puts doc.to_s}
+#    puts content.size.to_s
   end
 end
 
@@ -44,5 +67,4 @@ end
 a = Indexer.new
 #puts a.getSW.size.to_s
 a.index_words('Docs/Diald-HOWTO.txt')
-
-
+#puts a.getIndexedTerms
