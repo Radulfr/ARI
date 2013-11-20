c3e17264c270c42c634604e0f2b2cae375460a24
Radulfr <raul.elfriky@gmail.com>
2013-11-20 17:20:00 +0100

    all words indexed

--- indexer.rb
+++ indexer.rb
@@ -11,7 +11,8 @@ class Indexer
     @docs       = @db.collection("documents")
     @termscoll  = @db.collection("terms")
     @sw         = getSW
-    @terms      = getIndexedTerms
+    @all_files  = Dir.entries('Docs') - ['.', '..']
+#    @terms      = getIndexedTerms
     @indexed    = Array.new
   end
 
@@ -23,7 +24,6 @@ class Indexer
     else
       indexedTerms.size.times { |i| @terms[i] = indexedTerms[i].to_s.scan(/"[a-z]+"=>/)}
       @terms.size.times { |i| @terms[i].to_s.scan(/\w+/){ |w| @terms[i] = w} }
-#      @terms = @terms - [ "^[a-z0-9]+" ]
       return @terms
     end
   end
@@ -33,13 +33,13 @@ class Indexer
     @sw = Array.new
     stopWords = @stopwords.find.to_a
     stopWords.size.times { |i| @sw[i] = stopWords[i].to_s.scan(/=>"[a-z]+"/)}
-#    @sw.size.times { |i| @sw[i] = @sw[i].to_s.scan(/\w+/)}
     @sw.size.times { |i| @sw[i].to_s.scan(/\w+/){ |w| @sw[i] = w} }
     return @sw
   end
 
-  def index_words(document)
-    puts ">>> Processing document: " + document.to_s
+  #Indexing words of a document
+  def index_words(document, index)
+    puts "["+index.to_s+"]\t" + "Processing document: " + document.to_s
     content = ""
     File.open(document) do |doc|
       while linea = doc.gets
@@ -49,43 +49,35 @@ class Indexer
     content.encode!('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
     content = content.scan(/\S\w+\D/)
     content.size.times { |i| content[i] = content[i].gsub(/\p{^Alnum}/, '').to_s.downcase}
-#    @sw.size.times { |i| puts @sw[i].to_s + " - " + content[i].to_s}
 
     #Deleting StopWords
     content = content - @sw
+
+    #warning
     self.getIndexedTerms
 
     #Deleting repeated
     content = content.uniq
-#    content = content - [ "^[a-z0-9]+" ]
-#    puts content.size.to_s
+
     @indexed += content
     @indexed = @indexed.uniq
+  end
 
-#    for i in 0..content.size-1
-#      if not @terms.include?(content[i])
-#        @termscoll.insert(content[i] => 0)
-#      else
-#      end
-#    end
-#    content.each { |doc| puts doc.to_s}
-#    puts content.size.to_s
+  #Count word in every doc
+  def count_word
+    self.getIndexedTerms
+  end
+
+  def start
+    @all_files.size.times { |i| index_words('Docs/'+@all_files[i], i+1)}
+    puts "Saving..."
+    @indexed.size.times { |i| @termscoll.insert(@indexed[i] => 0)}
+    puts "Done!"
   end
 end
 
-
-
 #Main
 a = Indexer.new
+a.start
 
-#for each document
-#a.index_words('Docs/Diald-HOWTO.txt')
 
-all_files = Dir.entries('Docs') - ['.', '..']
-
-#all_files.(size/8).times { |i| a.index_words('Docs/'+all_files[i])}
-
-puts @indexed.size.to_s
-puts "Saving..."
-@indexed.(size/8).times { |i| @termscoll.insert(@indexed[i] => 0)}
-puts "Done! "
