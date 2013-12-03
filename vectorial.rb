bef542693c3d8d3b75a9bf48468f70cb962932f8
Radulfr <raul.elfriky@gmail.com>
2013-12-03 19:21:16 +0100

    Vectorial alpha version (debuggin')

--- vectorial.rb
+++ vectorial.rb
@@ -57,7 +57,6 @@ class Vectorial
     for i in 0..docs_names.size-1
       for j in 0..terms.size-1
         for k in 0..terms[j].size-1
-#          puts docs_names[i].to_s+ "--------->"+ terms[j][k][0].to_s
           if docs_names[i].eql?(terms[j][k][0])
             data[i][j] = terms[j][k][1].to_i
             puts "Im here -> " + data[i][j].to_s
@@ -69,14 +68,11 @@ class Vectorial
         end
       end
     end
-#    terms.each{ |term| puts term, "----------------" }
-
     puts "# Docs: " + docs_names.size.to_s
     puts "# Terms: "+ terms.size.to_s
     return data
   end
 
-  #Design Under Test
   def getData
     docs = Array.new
     docs_names = Array.new
@@ -95,26 +91,76 @@ class Vectorial
     docs_names = docs_names - ['_id', 'term']
     docs_names = docs_names.uniq
 
-#-------------
     data = Array.new(docs_names.size) { Array.new(terms.size, 0) }
-
+    data_w = Array.new(docs_names.size) { Array.new(terms.size, 0) }
+#INITIAL -> data
     for i in 0..docs_names.size-1
       for j in 0..terms.size-1
         for k in 0..terms[j].size-1
           if docs_names[i].eql?(terms[j][k][0])
             data[i][j] = terms[j][k][1]
-#            print data, " ", terms[j][k][0],"\n"
           end
         end
       end
     end
-#    terms.each{ |term| puts term, "----------------" }
-#    print terms
-    puts "# Docs: " + docs_names.size.to_s
-    puts "# Terms: "+ terms.size.to_s
-    return data
+#CONTAR.SI
+    count_if = Array.new(@question.size, 0)
+    log_count_if = Array.new(@question.size, 0)
+    for j in 0..docs_names.size-1
+      for i in 0..@question.size-1
+        count_if[i] += data[j][i]
+      end
+    end
+#    print count_if
+#LOGARITHM count_if
+#--------------------------------
+    log_count_if = Array.new(count_if.size, 0)
+    for i in 0..count_if.size-1
+      #WARNING HERE
+      log_count_if[i] = Math.log10(count_if[i]) 
+    end
+#DATA_W--------------------------------
+
+    for i in 0..docs_names.size-1
+      for j in 0..terms.size-1
+        data_w[i][j] = data[i][j]/log_count_if[j]
+      end
+    end
+
+#SUM---------------------------------
+    sum = Array.new(docs_names.size,0)
+    for i in 0..docs_names.size-1
+      for j in 0..terms.size-1
+        sum[i] += data_w[i][j]
+      end
+    end
+#SUM---------------------------------
+    e1 = Array.new(docs_names.size,0)
+
+    for i in 0..docs_names.size-1
+      for j in 0..terms.size-1
+        e1[i] += (data_w[i][j])**2
+      end
+    end
+    sum.each_index { |i| e1[i] += sum[i]**2 } 
+#E2---------------------------------
+
+    e2 = e1.collect{|item|  item*terms.size}
+#SUMA/E2----------------------------
+    ranking = Array.new(docs_names.size,0)
+    results = Array.new(docs_names.size) { Array.new(2) }
+
+    for i in 0..e1.size-1
+      results[i][0] = docs_names[i]
+      results[i][1] =  sum[i]/e2[i]
+    end
+
+    results = results.sort_by{|e| e[1]}
+    results = results.reverse
+    return results
   end
-  def start
+#------------------------------------------ 
+ def start
     puts "==== QUESTION ===="
     getQuestion
     puts "==== RESULTS ===="
@@ -125,9 +171,12 @@ end
 
 a = Vectorial.new
 puts "==== QUESTION ===="
-puts a.getQuestion("I have a compiler of ruby")
+puts a.getQuestion("I have a ruby compiler")
+#puts a.getQuestion("ruby flesh")
 puts "==== RESULTS ===="
 data = a.getData
 print data, "\n"
 #a.DUT
 #docs.each { |entry| puts entry, "----" }
+
+
