3dbb3cbc1579699b216f57f7d2da232f97a932d5
Radulfr <raul.elfriky@gmail.com>
2013-12-03 10:40:04 +0100

    building initial table...

--- vectorial.rb
+++ vectorial.rb
@@ -35,14 +35,37 @@ class Vectorial
   def getDocuments
     docs = Array.new
     docs_names = Array.new
+    terms = Array.new
+    data = Array.new
+
     @question.each { |word| docs += (@postings.find_one({:term => word}).to_a) }
+
+    for i in 0..@question.size-1
+      terms[i] = @postings.find_one({:term => @question[i]}).to_a
+    end
 
     for i in 0..docs.size-1
       docs_names[i] = docs[i][0]
     end
     docs_names = docs_names - ['_id', 'term']
-
-    return docs_names
+ 
+#need to refine TERMS   
+#    puts docs_names[1], terms[1][0]
+    for i in 0..docs_names.size-1
+      data [i] = Array.new
+      for j in 0..terms.size-1
+        puts terms[j][0][0].to_s+ "--->"+ docs_names[i].to_s
+        if terms[j][0][0].eql?(docs_names[i])
+          data[i][j] = terms[j][1]
+        else
+          data[i][j] = 0
+        end
+      end
+    end
+    terms.each{ |term| puts term[0][1], "----------------" }
+    puts "# Docs: " + docs_names.size.to_s
+    puts "# Terms: "+ terms.size.to_s
+    return data
   end
 
   #Design Under Test
